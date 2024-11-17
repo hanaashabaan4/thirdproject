1b3c091206c5db0a175f5cb06c57399144549cf2
hanaashabaan4 <hanaashanan4@gmail.com>
2024-11-17 02:38:08 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,116 @@
+
+  // /////////////////////////////////////////////////////
+
+  const button = document.querySelectorAll('[title="coppied"]')
+  console.log(button)
+button.forEach((e)=>{
+  e.addEventListener('click', () => {
+    alert('https://github.com/hanaashabaan4/-\n تم نسخ النص');  
+});
+})
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  // var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  //   return new bootstrap.Tooltip(tooltipTriggerEl)
+  // })
+  
+  // const button1 = document.getElementById('myButton2');
+  // const tooltip1 = document.getElementById('tooltip2');
+  // button1.addEventListener('click', () => {
+  //     alert('https://github.com/hanaashabaan4/-\n تم نسخ النص');  
+  // });
+  
+  // var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip2"]'))
+  // var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  //   return new bootstrap.Tooltip(tooltipTriggerEl)
+  // })
+  
+  // const button2 = document.getElementById('myButton3');
+  // const tooltip2 = document.getElementById('tooltip3');
+  // button2.addEventListener('click', () => {
+  //     alert('https://github.com/hanaashabaan4/-\n تم نسخ النص');  
+  // });
+  
+  // var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip3"]'))
+  // var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  //   return new bootstrap.Tooltip(tooltipTriggerEl)
+  // })
+
+
+// var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+// var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//   return new bootstrap.Tooltip(tooltipTriggerEl)
+// })
+// const button = document.getElementById('myButton');
+// button.addEventListener('click', () => {
+//     navigator.clipboard.writeText('https://github.com/hanaashabaan4/-') // نسخ الرابط للحافظة
+//         .then(() => {
+//             alert('تم نسخ الرابط إلى الحافظة!');
+//         })
+//         .catch(err => {
+//             console.error('خطأ في النسخ:', err);
+//         });
+// });
